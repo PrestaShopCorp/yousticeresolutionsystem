680beda4ebfc3402dc8ccc7b410d1ce72ea24ad2
devyoustice <dev@youstice.com>
2015-12-21 12:56:45 +0100

    minor language change

--- yousticeresolutionsystem.php
+++ yousticeresolutionsystem.php
@@ -19,7 +19,7 @@ class YousticeResolutionSystem extends Module
     {
         $this->name = 'yousticeresolutionsystem';
         $this->tab = 'advertising_marketing';
-        $this->version = '1.11.3';
+        $this->version = '1.11.4';
         $this->author = 'Youstice';
         $this->need_instance = 0;
         $this->ps_versions_compliancy = array('min' => '1.5', 'max' => '1.6');
